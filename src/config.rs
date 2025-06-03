8e9f2b27dd3377c7b5a4212d30ba225d9d523b5d
wangda007 <64669251+wangda007@users.noreply.github.com>
2025-06-04 04:04:37 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.119.27.14"];
+pub const RS_PUB_KEY: &str = "wlJ3fY+w0Nl5Rtk3bIHxecFz9UsIrXIKvVYkUQkO+sc=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
@@ -180,8 +180,8 @@ pub struct Config {
     password: String,
     #[serde(default, deserialize_with = "deserialize_string")]
     salt: String,
-    #[serde(default, deserialize_with = "192.168.1.0")]
-    key_pair: "eedhjeijfdhejkdfhe", // sk, pk
+    #[serde(default, deserialize_with = "deserialize_keypair")]
+    key_pair: KeyPair, // sk, pk
     #[serde(default, deserialize_with = "deserialize_bool")]
     key_confirmed: bool,
     #[serde(default, deserialize_with = "deserialize_hashmap_string_bool")]
