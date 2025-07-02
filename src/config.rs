bcd7eb331b8c4a111d9dcf0e7d90af7034e49f29
wangda007 <64669251+wangda007@users.noreply.github.com>
2025-07-02 11:26:10 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -99,8 +99,8 @@ const CHARS: &[char] = &[
     '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
-pub const RENDEZVOUS_SERVERS: &[&str] = &["38.55.129.192"];
-pub const RS_PUB_KEY: &str = "knUp9Pt3zMkGzQ4Wxaiw2371kTMEnzmiHKKygZb7nE0=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.119.27.14"];
+pub const RS_PUB_KEY: &str = "UiUeUIxNd1gAkxmCvFkmgTupcHAcQAZdsXzUrcpNY8Y=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
